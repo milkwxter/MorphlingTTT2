f7fe357de7ea3a9fcde1729040b827f5de72ea36
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-05 15:52:46 -0500

    Initial commit for working role

---
+++ ttt2-role_morph/lua/terrortown/entities/roles/morphling/shared.lua
@@ -0,0 +1,38 @@
+if SERVER then
+  AddCSLuaFile()
+
+  resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_morph.vmt")
+end
+
+function ROLE:PreInitialize()
+  self.color = Color(216, 24, 89, 255)
+
+  self.abbr = "morph" -- abbreviation
+  self.surviveBonus = 0.5 -- bonus multiplier for every survive while another player was killed
+  self.scoreKillsMultiplier = 5 -- multiplier for kill of player of another team
+  self.scoreTeamKillsMultiplier = -16 -- multiplier for teamkill
+  self.preventFindCredits = false
+  self.preventKillCredits = false
+  self.preventTraitorAloneCredits = false
+  
+  self.isOmniscientRole = true
+
+  self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
+  self.defaultTeam = TEAM_TRAITOR
+
+  self.conVarData = {
+    pct = 0.17, -- necessary: percentage of getting this role selected (per player)
+    maximum = 1, -- maximum amount of roles in a round
+    minPlayers = 6, -- minimum amount of players until this role is able to get selected
+    credits = 1, -- the starting credits of a specific role
+    togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+    random = 50,
+    traitorButton = 1, -- can use traitor buttons
+    shopFallback = SHOP_FALLBACK_TRAITOR
+  }
+end
+
+-- now link this subrole with its baserole
+function ROLE:Initialize()
+  roles.SetBaseRole(self, ROLE_TRAITOR)
+end
---
+++ ttt2-role_morph/lua/terrortown/lang/en/morphling.lua
@@ -0,0 +1,9 @@
+local L = LANG.GetLanguageTableReference("en")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[MORPHLING.name] = "Morphling"
+L["info_popup_" .. MORPHLING.name] = [[You are the Morphling! Disguise yourself as other players to cause chaos.]]
+L["body_found_" .. MORPHLING.abbr] = "They were a Morphling."
+L["search_role_" .. MORPHLING.abbr] = "This person was a Morphling!"
+L["target_" .. MORPHLING.name] = "Morphling"
+L["ttt2_desc_" .. MORPHLING.name] = [[The Morphling can disguise himself as any player in the lobby.]]
