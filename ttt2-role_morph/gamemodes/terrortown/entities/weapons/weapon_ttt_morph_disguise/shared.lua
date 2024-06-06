a383a42a1e5b3dd99862d1f15558d8155da2f9bc
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-05 19:22:44 -0500

    No more errors

--- ttt2-role_morph/gamemodes/terrortown/entities/weapons/weapon_ttt_morph_disguise/shared.lua
+++ ttt2-role_morph/gamemodes/terrortown/entities/weapons/weapon_ttt_morph_disguise/shared.lua
@@ -59,41 +59,43 @@ function morphDisguiseFunction(nickname)
    util.Effect("AntlionGib", edata)
 end
 
-function SWEP:PrimaryAttack()
-   -- Create a GUI and sound
-   morphFrame = vgui.Create("DFrame")
-	morphFrame:SetPos(10, ScrH() - 800)
-	morphFrame:SetSize(200, 300)
-	morphFrame:SetTitle("Disguise into: (Hold " .. Key("+showscores", "tab"):lower() .. ")")
-	morphFrame:SetDraggable(true)
-	morphFrame:ShowCloseButton(true)
-	morphFrame:SetVisible(true)
-	morphFrame:SetDeleteOnClose(true)
-	surface.PlaySound("npc/antlion/attack_single1.wav")
+if CLIENT then
+   function SWEP:PrimaryAttack()
+      -- Create a GUI and sound
+      morphFrame = vgui.Create("DFrame")
+      morphFrame:SetPos(10, ScrH() - 800)
+      morphFrame:SetSize(200, 300)
+      morphFrame:SetTitle("Disguise into: (Hold " .. Key("+showscores", "tab"):lower() .. ")")
+      morphFrame:SetDraggable(true)
+      morphFrame:ShowCloseButton(true)
+      morphFrame:SetVisible(true)
+      morphFrame:SetDeleteOnClose(true)
+      surface.PlaySound("npc/antlion/attack_single1.wav")
 
-	-- Create list of Players avaliable to disguise into
-	local list = vgui.Create("DListView", morphFrame)
-	list:Dock(FILL)
-	list:SetMultiSelect(false)
-	list:AddColumn("Players")
+      -- Create list of Players avaliable to disguise into
+      local list = vgui.Create("DListView", morphFrame)
+      list:Dock(FILL)
+      list:SetMultiSelect(false)
+      list:AddColumn("Players")
 
-   -- Populate the list
-	for _, v in ipairs(player.GetAll()) do
-		if (v:Alive() and not v:IsSpec()) or not v:Alive() then
-			list:AddLine(v:Nick())
-		end
-	end
+      -- Populate the list
+      for _, v in ipairs(player.GetAll()) do
+         if (v:Alive() and not v:IsSpec()) or not v:Alive() then
+            list:AddLine(v:Nick())
+         end
+      end
 
-   -- When player selects one of the options, do X
-	list.OnRowSelected = function(lst, index, pnl)
-		local ply = LocalPlayer()
-		if ply:Alive() and not ply:IsSpec() then
-         -- Remind player who they disguised into
-         LocalPlayer():PrintMessage(HUD_PRINTTALK, "You disguised into: " .. pnl:GetValue(1))
-			morphDisguiseFunction(pnl:GetValue(1))
-         morphFrame:Close()
-		else
-			ply:PrintMessage(HUD_PRINTTALK, "Error. You must be alive to disguise.")
-		end
-	end
+      -- When player selects one of the options, do X
+      list.OnRowSelected = function(lst, index, pnl)
+         local ply = LocalPlayer()
+         if ply:Alive() and not ply:IsSpec() then
+            -- Remind player who they disguised into
+            LocalPlayer():PrintMessage(HUD_PRINTTALK, "You disguised into: " .. pnl:GetValue(1))
+            morphDisguiseFunction(pnl:GetValue(1))
+            morphFrame:Close()
+         else
+            ply:PrintMessage(HUD_PRINTTALK, "Error. You must be alive to disguise.")
+         end
+      end
+   end
 end
